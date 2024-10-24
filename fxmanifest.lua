598c85fd060e359de07785be5b5f610d9b71c02c
munafio <munafaqeelmahdi@gmail.com>
2024-10-24 21:45:23 +0300

    version updated

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version "adamant"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 games {"rdr3"}
 
-version '1.0.0'
+version '1.2.0'
 author 'BLN Studio - RedM'
 
 lua54 'yes'
